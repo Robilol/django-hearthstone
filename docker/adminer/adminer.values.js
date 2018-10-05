2ffaef431f06c8e9dbf77b3c7219d08677f4aa2d
Robilol <robin.regis78@gmail.com>
2018-10-05 11:54:10 +0200

    remove idea

--- docker/adminer/adminer.values.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    plugins: 'tables-filter tinymce',
-    design: 'pepa-linha',
-    port: '8080',
-    container_name: 'adminer_2rlg45q1jmvtn7vk',
-};
--- docker/mailhog/mailhog.values.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    smtp_server_port: '1025',
-    web_server_port: '8025',
-    container_name: 'mailhog_2rlg45q1jmvtn7vm',
-};
